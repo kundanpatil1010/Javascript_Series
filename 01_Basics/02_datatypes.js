a4a1141b72f3067b5848d8d74448c95c133dc6a1
kundanpatil1010 <132280310+kundanpatil1010@users.noreply.github.com>
2024-07-24 13:20:44 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+//datatypes
+"use strict"; //treat all JS Code as new version
+//alert(3+3) //we are using node js not browser
+console.log();
+//code readability should be High
+//www.tc39.cs documentation
+let name="kundan"
+let age=18
+let isLoggedIn=false
+let state;
+//number=.2 to power 53
+//bigint
+//string
+//boolean=>true/false
+//null =>standolone value
+//undefined=>
+//symbol=> to find unique 
+
+//*object
+console.log(typeof("kundan"));
+console.log(typeof(null)); // object
+console.log(typeof(undefined)); // undefined
+
+
+
+
+
+
+
