7b97c6036270c20946df7aad57d7c6e6115cd12c
kundanpatil1010 <132280310+kundanpatil1010@users.noreply.github.com>
2024-07-24 13:44:22 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+//Conversion Operation
+/*
+let score="true";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+"33"=>33
+"33abs"=>NaN
+true=1 & false=0
+
+*/
+//let isLoggedIn =0 false
+//let isLoggedIn =1 true
+//let isLoggedIn ="kp" true
+//let isLoggedIn ="" false
+let isLoggedIn ="kp"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber=69
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+
+
+
+
